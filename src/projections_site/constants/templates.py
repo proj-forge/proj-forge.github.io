e8b815e19cb27d4e166a7fc77c4e48767481cd88
Matt Fisher <mfisher87@gmail.com>
2023-10-22 16:56:41 -0600

    Add pprint jinja global

--- src/projections_site/constants/templates.py
+++ src/projections_site/constants/templates.py
@@ -1,10 +1,18 @@
 from __future__ import annotations
 
+from pprint import pprint
+
 from jinja2 import Environment, PackageLoader, select_autoescape
 
 env = Environment(
     loader=PackageLoader("projections_site"),
     autoescape=select_autoescape(),
+)
+# TODO: Why can't this be set at init-time??
+env.globals.update(
+    {
+        "pprint": pprint,
+    }
 )
 
 TEMPLATES = {
