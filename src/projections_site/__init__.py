d5bf183408768ffd2b3c70a6d22a8d9f8566ea7d
Matt Fisher <mfisher87@gmail.com>
2023-10-22 14:02:54 -0600

    Init from Scientific Python template

---
+++ src/projections_site/__init__.py
@@ -0,0 +1,12 @@
+"""
+Copyright (c) 2023 PyMRA. All rights reserved.
+
+projections-site: A site for displaying rich information about coordinate reference systems. Like epsg.io, except for all coordinate reference systems in Proj.
+"""
+
+
+from __future__ import annotations
+
+from ._version import version as __version__
+
+__all__ = ("__version__",)
---
+++ tests/test_package.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+import importlib.metadata
+
+import projections_site as m
+
+
+def test_version():
+    assert importlib.metadata.version("projections_site") == m.__version__
